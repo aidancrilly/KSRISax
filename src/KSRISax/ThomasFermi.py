d40c05c559820364187176416d017cae44a8d3f2
Aidan Crilly <20111148+aidancrilly@users.noreply.github.com>
2026-03-07 00:43:55 +0000

    Update ThomasFermi.py

--- src/KSRISax/ThomasFermi.py
+++ src/KSRISax/ThomasFermi.py
@@ -93,6 +93,6 @@ class ThomasFermiSolver(eqx.Module):
         Epot = 0.0
         P = (2.0 / 9.0) * (N / V) * T * (b**3 / alpha) * fermi_dirac_integral_three_half(beta[-1] / b) * gamma5h
         U = 1.5 * P + 0.5 * Epot / V
-        mu = T * beta[-1]
+        mu = T * (beta[-1] / b)
         return {'U' : U, 'P' : P, 'mu' : mu, 'beta(0)' : beta[0]}
 
